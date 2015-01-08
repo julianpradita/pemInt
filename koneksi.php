53654f79cef48d8887d3d05497778755b9739f83
julianpradita <julianmail07@gmail.com>
2015-01-08 14:57:53 +0700

    new

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,4 @@
 <?php
-$host = "localhost";
-$username = "a7128970_job123";
-$password = "a7128970_job123";
-
 mysql_connect('mysql7.000webhost.com','a7128970_job123','job123');
 mysql_select_db('a7128970_job');
 ?>
